8858294cfe47f1eae198bbdf66c011db6fb98e17
Tushar Kadlag <tusharkadlag81@gmail.com>
2020-11-02 19:29:22 +0000

    Update dynamodb.py

--- dynamodbgeo/dynamodb.py
+++ dynamodbgeo/dynamodb.py
@@ -28,36 +28,3 @@ table_util.create_table(create_table_input)
 dub_lat2 = 53.339428
 dub_lon2 = -6.257664
 
-for line in open('customer.json', 'r'):
-        data = (json.loads(line))
-        print(data)
-        lat1 = data['latitude']
-        print(lat1)
-
-        lon1 = data['longitude']
-        print(lon1)
-        
-
-       	PutItemInput = {
-            'Item': {
-                'user_id': {'N':str(data['user_id'])},
-                'name': {'S': data['name']}
-
-            }
-
-	}
-	geoDataManager.put_Point(dynamodbgeo.PutPointInput(
-            dynamodbgeo.GeoPoint(float(data['latitude']),float(data['longitude'])),  # latitude then latitude longitude
-            str(uuid.uuid4()),  # Use this to ensure uniqueness of the hash/range pairs.
-            PutItemInput  # pass the dict here
-        ))
-
-print('debug')
-QueryRadiusInput={    }
-
-query_reduis_result=geoDataManager.queryRadius(
-    dynamodbgeo.QueryRadiusRequest(
-        dynamodbgeo.GeoPoint(53.339428, -6.257664), # center point and radius is in meters
-        100000, QueryRadiusInput, sort = True)) # diameter
-print(query_reduis_result)
-
